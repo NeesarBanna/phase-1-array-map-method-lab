aa5d13101e2f26658e3004db1b86d8ae1842c741
Neesar Banna <Bannasqe@gmail.com>
2024-07-22 13:19:28 -0400

    array map method lab

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+function titleCased() {
+  return tutorials.map(function(title) {
+    return title.split(' ').map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
+
+console.log(titleCased());
